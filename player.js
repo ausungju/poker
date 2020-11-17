8586bbdde200dde116376d3f4691998f1b7afaf0
ausungju <58778326+ausungju@users.noreply.github.com>
2020-11-17 23:42:00 +0900

    Update player.js

--- player.js
+++ player.js
@@ -2,20 +2,27 @@ window.onload = function(){
 	
 };
 // 사용자의 정보를 저장한 객체
-function User(LOAD = NULL,name){
+function User(LOAD = null,name = ""){
 	// 사용자의 이름, 죽은 휫수, 돈의 변수 선언
 	this.name = name;
-	this.numOfDie =numOfDie;
-	this.money = money;
+	var numOfDie = 0;
+	this.money = 1000000;
 	
 	this.thisDeck = [];
 	
 	// 만약 사용자가 플레이한 기록이 있을시 불러오기
-	if(LOAD != NULL){
+	if(LOAD != null){
+		if(LOAD.numOfDie > 0) return;
 		this.name = LOAD.name;
 		this.numOfDie = LOAD.numOfDie;
 		this.money = LOAD.money;
 		};
+};
+User.prototype.save = function(){
+	saveToFile(this.name + ".js", (
+	"var " + this.name + " = { name: \"" + this.name + "\", numOfDie: " 
+	+ this.numOfDie + ", money: " + this.money + "};"
+	))
 };
 // npc
 function nonUser(name, money){
@@ -24,3 +31,31 @@ function nonUser(name, money){
 	this.numOfDie = 0;
 	this.thisDeck = [];
 };
+
+var die = function(Unit){
+	if(Unit.money < 0) Unit.numOfDie++;
+};
+
+
+var you = {name: "my", numOfDie: 0, money: 100000};
+var me = new User(you);
+console.log(me);
+me.save();
+
+
+
+function saveToFile(fileName, content) {
+    var blob = new Blob([content], { type: 'text/plain' });
+    objURL = window.URL.createObjectURL(blob);
+            
+    // 이전에 생성된 메모리 해제
+    if (window.__Xr_objURL_forCreatingFile__) {
+        window.URL.revokeObjectURL(window.__Xr_objURL_forCreatingFile__);
+    }
+    window.__Xr_objURL_forCreatingFile__ = objURL;
+    var a = document.createElement('a');
+    a.download = fileName;
+    a.href = objURL;
+    a.click();
+};
+
