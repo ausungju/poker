685e2d06a88982590ad393bee8f0b58f0761dc09
ausungju <58778326+ausungju@users.noreply.github.com>
2020-11-24 23:58:20 +0900

    Update player.js

--- player.js
+++ player.js
@@ -1,4 +1,7 @@
 window.onload = function(){
+	FileName = document.createElement("script");
+	var element = document.head;
+	element.appendChild(FileName);
 	
 };
 // 사용자의 정보를 저장한 객체
@@ -19,11 +22,28 @@ function User(LOAD = null,name = ""){
 		};
 };
 User.prototype.save = function(){
-	saveToFile(this.name + ".js", (
-	"var " + this.name + " = { name: \"" + this.name + "\", numOfDie: " 
-	+ this.numOfDie + ", money: " + this.money + "};"
-	))
+	
+	function saveToFile(fileName, content) {
+		var blob = new Blob([content], { type: 'text/plain' });
+		objURL = window.URL.createObjectURL(blob);
+            
+		// 이전에 생성된 메모리 해제
+		if (window.__Xr_objURL_forCreatingFile__) {
+			window.URL.revokeObjectURL(window.__Xr_objURL_forCreatingFile__);
+		}
+		window.__Xr_objURL_forCreatingFile__ = objURL;
+		var a = document.createElement('a');
+		a.download = fileName;
+		a.href = objURL;
+		a.click();
+	}
+
+	var fileName = this.name + ".js"
+	var content = "var " + "LOAD" + " = { name: \"" + this.name + "\", numOfDie: " 
+	+ this.numOfDie + ", money: " + this.money + "};";
+	saveToFile(fileName, content);
 };
+
 // npc
 function nonUser(name, money){
 	this.name = name;
@@ -39,41 +59,35 @@ var die = function(Unit){
 
 var you = {name: "my", numOfDie: 0, money: 100000};
 var me = new User(you);
-//me.save();
+me.save();
 
 
 
-function saveToFile(fileName, content) {
-    var blob = new Blob([content], { type: 'text/plain' });
-    objURL = window.URL.createObjectURL(blob);
-            
-    // 이전에 생성된 메모리 해제
-    if (window.__Xr_objURL_forCreatingFile__) {
-        window.URL.revokeObjectURL(window.__Xr_objURL_forCreatingFile__);
-    }
-    window.__Xr_objURL_forCreatingFile__ = objURL;
-    var a = document.createElement('a');
-    a.download = fileName;
-    a.href = objURL;
-    a.click();
-};
 
 function openTextFile() {
-    var input = document.createElement("input");
-    input.type = "file";
-    input.accept = "text/plain, text/html, .js"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
-    input.onchange = function (event) {
-        processFile(event.target.files[0]);
-    };
-    input.click();
-}
+	var getFile = "";
+	
+	var processFile = function(file){
+		var reader = new FileReader();
+		reader.onload = function () {
+			var userName = file.name.match(/.+\./).join().slice(0,-1);
+			if (window.confirm(userName + "님이 맞습니까?")) { 
+				getFile = file.name;
+				output.innerText = file.name;
+				FileName.setAttribute("src", getFile);
+				console.log(document.head);
+				alert(userName + "님의 정보를 불러왔습니다");
+			}else alert("다시 시도하세요!")
+		}
+	reader.readAsText(file, /* optional */ "euc-kr");
+	}
+	
+	var input = document.createElement("input");
+	input.type = "file";
+	input.accept = "text/plain, text/html, .js"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
+	input.onchange = function (event) {
+		processFile(event.target.files[0]);
+	};
 
-function processFile(file) {
-    var reader = new FileReader();
-    reader.onload = function () {
-        output.innerText = reader.result;
-		console.log(file.name);
-    };
-    reader.readAsText(file, /* optional */ "euc-kr");
-}
-hi();
+	input.click();
+};
