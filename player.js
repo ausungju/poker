458f058df1739061d7756e44e3b342fab84fa2d3
ausungju <58778326+ausungju@users.noreply.github.com>
2020-11-25 23:44:22 +0900

    Update player.js

--- player.js
+++ player.js
@@ -1,26 +1,22 @@
-window.onload = function(){
-	FileName = document.createElement("script");
-	var element = document.head;
-	element.appendChild(FileName);
-	
-};
 // 사용자의 정보를 저장한 객체
 function User(LOAD = null,name = ""){
-	// 사용자의 이름, 죽은 휫수, 돈의 변수 선언
 	this.name = name;
-	var numOfDie = 0;
+	this.numOfDie = 0;
 	this.money = 1000000;
 	
 	this.thisDeck = [];
-	
+
+
 	// 만약 사용자가 플레이한 기록이 있을시 불러오기
 	if(LOAD != null){
 		if(LOAD.numOfDie > 0) return;
 		this.name = LOAD.name;
 		this.numOfDie = LOAD.numOfDie;
 		this.money = LOAD.money;
-		};
+	}
 };
+
+// 저장하기
 User.prototype.save = function(){
 	
 	function saveToFile(fileName, content) {
@@ -44,39 +40,52 @@ User.prototype.save = function(){
 	saveToFile(fileName, content);
 };
 
-// npc
-function nonUser(name, money){
-	this.name = name;
-	this.money = money;
-	this.numOfDie = 0;
-	this.thisDeck = [];
+// 새로하기
+function newUser(){
+	var getUserName = prompt("닉네임을 입력하시오","홍길동");
+	// 게임 시작유무 확인
+	if (confirm(getUserName+ "님 \n게임을 시작하시겠습니까?")) {
+		user = new User(null,getUserName);	
+		console.log(user);
+		alert("게임을 시작합니다.");
+	}
 };
 
-var die = function(Unit){
-	if(Unit.money < 0) Unit.numOfDie++;
+// 이어하기
+function newGame(){
+	// 받은 데이터의 유무
+	if(document.getElementsByTagName("script").length == 3){
+		alert("불러온 데이터가 없습니다.")
+	}
+	// 받은 데이터가 파산한 테이터인지 확인
+	else if(LOAD.numOfDie > 0){
+		alert(LOAD.name + "님은 이미 파산 하였습니다.")
+	}
+	// 데이터 적용
+	else {
+		user = new User(LOAD);
+		alert("게임을 시작합니다.");
+	}
 };
 
-
-var you = {name: "my", numOfDie: 0, money: 100000};
-var me = new User(you);
-me.save();
-
-
-
-
+// 불러오기
 function openTextFile() {
 	var getFile = "";
 	
 	var processFile = function(file){
 		var reader = new FileReader();
+		
 		reader.onload = function () {
 			var userName = file.name.match(/.+\./).join().slice(0,-1);
-			if (window.confirm(userName + "님이 맞습니까?")) { 
-				getFile = file.name;
-				output.innerText = file.name;
-				FileName.setAttribute("src", getFile);
-				console.log(document.head);
-				alert(userName + "님의 정보를 불러왔습니다");
+			
+			if (confirm(userName + "님이 맞습니까?")) { 
+				getFile = file.name;			
+				var newScript = document.createElement("script");
+				newScript.setAttribute("src", getFile);
+				var element = document.head;
+				element.appendChild(newScript);
+				output.innerText = userName +"님 환영합니다.";
+				alert(userName + "님의 정보를 불러왔습니다");	
 			}else alert("다시 시도하세요!")
 		}
 	reader.readAsText(file, /* optional */ "euc-kr");
@@ -87,7 +96,31 @@ function openTextFile() {
 	input.accept = "text/plain, text/html, .js"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
 	input.onchange = function (event) {
 		processFile(event.target.files[0]);
-	};
+	}
 
 	input.click();
 };
+
+// npc
+function nonUser(name, money){
+	this.name = name;
+	this.money = money;
+	this.numOfDie = 0;
+	this.thisDeck = [];
+};
+
+// 플레이어의 사망 유무 확인
+var die = function(Unit){
+	if(Unit.money < 0) Unit.numOfDie++;
+};
+
+window.onload = function(){
+	// User 데이터 기본값 설정
+	var user = new User;
+};
+
+
+
+
+
+
