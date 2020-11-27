e2ecc3bb14d523003ef2b784b439319708587da1
ausungju <58778326+ausungju@users.noreply.github.com>
2020-11-27 23:59:23 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,23 +1,58 @@
-var newPage = function(){
+var newPage = function( get, seed ){
 	var page = document.body;
-
-	while ( page.firstChild ) { 
+	
+	while ( page.firstChild ) {
+		var ppage = page.firstChild
+		if(page.firstChild == ""){
+			console.log("앙ㄴ녕")
+		}
+		console.log(page.firstChild);
+		if(document.body.getAttribute("id") == "default"){
+			continue;
+		}
 		page.removeChild( page.firstChild ); 
 	}
+	switch( get ){
+		case "main" : {
+		createNode( "button", "저장", undefined, [ ["onclick", "user.save()"], ["id", "save"] ] )
+		createNode( "button", "시작", undefined, [ ["onclick", "newPage(\"game\")"], ["id", "start"] ] )
+		createNode("div", (user.name + ",  Money : " + user.money), undefined, [ ["id", ("state")] ] );
+		break;
+		}
+		case "game" : {
+			game = new Game( seed )
+			var List = game.playerList;
+			
+			createNode( "div", game.seed, undefined, [ ["id", "seed"] ] );
+			createNode( "div", game.seed, undefined, [ ["id", "seed"] ] );
+			for(var i in List){
 
-	var div1 = document.createElement("div");
-	div1.setAttribute("onclick", "console.log(\"안녕\");");
-	var divnode = document.createTextNode("안녕");
-	div1.appendChild( divnode );
-	page.appendChild(div1);
-}
+				createNode("div", (List[i].name + ",  Money : " + List[i].money), undefined, [ ["class", ("player")] ] );
+			}
+			console.log(document.body);
+			break;
+		}
+		
+	}
+};
+var createNode = function(tag, textNode, stand = document.body, Attribute = []){
+	var element = document.createElement( tag );
+	var node = document.createTextNode( textNode );
+	if(Attribute != []){
+		for(var i in Attribute){
+			element.setAttribute( Attribute[i][0], Attribute[i][1] );
+		}
+	}
+	element.appendChild( node );
+	stand.appendChild( element );
+};
 	
 	
-function game(seed){
+function Game(seed = 5000){
 
-	var playerList =[ 
+	this.playerList = [ 
 		user,new nonUser("A",10000), new nonUser("B",10000),
-		new nonUser("C",10000), new nonUser("C",10000)
+		new nonUser("C",10000), new nonUser("D",10000)
 	];
 	
 	var seed = this.seed = seed;
